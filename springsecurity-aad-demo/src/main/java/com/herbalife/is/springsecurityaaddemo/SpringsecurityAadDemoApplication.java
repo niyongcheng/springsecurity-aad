cd36e106487ce00766f8f3ee3c103f9f919faa8b
sid.ni <sid.ni@ef.com>
2020-05-25 22:56:18 +0800

    #add aad

---
+++ springsecurity-aad-demo/src/main/java/com/herbalife/is/springsecurityaaddemo/SpringsecurityAadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.herbalife.is.springsecurityaaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityAadDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityAadDemoApplication.class, args);
+	}
+
+}
---
+++ springsecurity-aad-demo/src/test/java/com/herbalife/is/springsecurityaaddemo/SpringsecurityAadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herbalife.is.springsecurityaaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityAadDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
